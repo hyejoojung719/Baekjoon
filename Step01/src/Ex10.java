ad8d4365181bb62aaf84ceb0b33140c9d9259b09
fixup <fixup719@gmail.com>
2022-02-22 16:21:06 +0900

    ex10 update

---
+++ Step01/src/Ex10.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ex10 {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		String id = sc.next();
+
+		System.out.println(id + "??!");
+
+		
+	}
+}
