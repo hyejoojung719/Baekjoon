dd567fdbdb0b752064457ed9ea56790c3bfc5ac5
fixup <fixup719@gmail.com>
2022-02-22 18:20:01 +0900

    ex13 update

--- Step01/src/Ex13.java
+++ Step01/src/Ex13.java
@@ -5,6 +5,13 @@ public class Ex13 {
 
 		Scanner sc = new Scanner(System.in);
 		
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		
+		System.out.println(num1*(num2%10));
+		System.out.println(num1*(num2%100/10));
+		System.out.println(num1*(num2/100));
+		System.out.println(num1*num2);
 		
 	}
 }
