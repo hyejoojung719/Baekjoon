3568dd6c0ba2845e3c9629465a1289f036c445a1
fixup <fixup719@gmail.com>
2022-02-23 17:39:38 +0900

    ex03~ex09 update

--- Step01/src/Ex03.java
+++ Step01/src/Ex03.java
@@ -1,4 +1,9 @@
 
 public class Ex03 {
-
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
 }
--- Step01/src/Ex04.java
+++ Step01/src/Ex04.java
@@ -1,4 +1,10 @@
 
 public class Ex04 {
-
+	public static void main(String[] args) {
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+	}
 }
