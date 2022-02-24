65be7fb7cde45d777834522af91120f49ae33672
fixup <fixup719@gmail.com>
2022-02-24 16:47:13 +0900

    ex11 배열사용, 나중에 buffer로도 풀기

---
+++ Step03/src/Ex11.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Ex11 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int x = sc.nextInt();
+		
+		int[] arr = new int[n];
+		
+		
+		for(int i = 0 ; i < n ; i++) {
+			int a = sc.nextInt();
+			arr[i] = a;
+		}
+		
+		for(int i = 0; i <arr.length; i++) {
+			if(arr[i] < x) {
+				System.out.print(arr[i] + " ");
+			}
+		}
+		
+	}
+}
