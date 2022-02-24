038d96cc081600aaaabecda9c508bd7e841dd7f9
fixup <fixup719@gmail.com>
2022-02-24 14:37:51 +0900

    ex02 update

---
+++ Step03/src/Ex02.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int testCase = sc.nextInt();
+		
+		
+		for(int i = 0 ; i < testCase ; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			System.out.println(a+b);
+		}
+		
+		
+		
+	}
+}
