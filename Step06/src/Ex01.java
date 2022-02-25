6c9db750b00d7e92998ba7e030a0d0d9deed5da3
fixup <fixup719@gmail.com>
2022-02-25 18:49:20 +0900

    ex01 update static정리

---
+++ Step06/src/Ex01.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Ex01 {
+	
+	public static long sum(int[] a ) {
+		
+		long ans=0;
+		for(int i=0;i<a.length;i++) {
+			ans += a[i];
+		}
+		return ans;
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int[] a = new int[n];
+		
+		for(int i=0;i<n;i++) {
+			a[i] = sc.nextInt();
+		}
+		
+		System.out.println(sum(a));
+	}
+}
