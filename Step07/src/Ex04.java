88d8f16c33dd3cf7cadffcd6feb51cc9244cb3c5
fixup <fixup719@gmail.com>
2022-02-27 18:38:48 +0900

    ex04 numberformat 런타임에러... 조심!

--- Step07/src/Ex04.java
+++ Step07/src/Ex04.java
@@ -1,16 +1,24 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Ex04 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
 		
-		int testCase = Integer.parseInt(sc.nextLine());
+		int testCase = Integer.parseInt(br.readLine());
 		
 		String[] strArr = new String[testCase];
 		int[] cntArr = new int[testCase];
 		for(int i=0; i<testCase; i++) {
-			cntArr[i] = Integer.parseInt(sc.nextLine());
-			strArr[i] = sc.nextLine();
+			st = new StringTokenizer(br.readLine(), " ");
+			cntArr[i] = Integer.parseInt(st.nextToken());
+			strArr[i] = st.nextToken();
 		}
 		
 		for(int i=0; i<testCase; i++) {
