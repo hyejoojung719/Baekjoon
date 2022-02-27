a6bf46ecec505a8fce3f10258423483722b4298d
fixup <fixup719@gmail.com>
2022-02-27 17:08:48 +0900

    ex01 update

---
+++ Step07/src/Ex01.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+// 아스키코드
+public class Ex01 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		char ch = sc.next().charAt(0);
+		
+		System.out.println((int)ch);
+	}
+}
