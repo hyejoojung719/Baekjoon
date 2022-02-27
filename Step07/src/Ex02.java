8c1daff38e737f03528bc64e30bd872548366cfd
fixup <fixup719@gmail.com>
2022-02-27 17:23:17 +0900

    ex02 update

---
+++ Step07/src/Ex02.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int size = Integer.parseInt(sc.nextLine());
+		
+		String str = sc.nextLine();
+	
+		char[] arr = new char[size];
+		for(int i=0; i<str.length(); i++) {
+			arr[i] = str.charAt(i);
+		}
+		
+		int sum=0;
+		for(int i=0; i<arr.length; i++) {
+			sum += ((int)arr[i]-48);
+		}
+		System.out.println(sum);
+	}
+}
