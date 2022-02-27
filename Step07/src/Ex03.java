dc5618ba1b3573ef14ac5d0a5c590c99e237ad53
fixup <fixup719@gmail.com>
2022-02-27 18:18:39 +0900

    ex03 update 쉬운것 같아 보여도 은근 샹각해야함

---
+++ Step07/src/Ex03.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Ex03 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.nextLine();
+		
+		//97~122
+		int[] chArr = new int[26];
+		for(int i=97; i<=122; i++) {
+			chArr[i-97] = i;
+		}
+		
+		//apple
+		for(int i=0; i<chArr.length; i++) {
+			for(int j=0; j<str.length(); j++) {
+				if((char)chArr[i] == str.charAt(j)) {
+					chArr[i]=j;
+					break;
+				}else if((j+1) == str.length()) {
+					chArr[i]=-1;
+					break;
+				}
+			}
+		}
+		
+		
+		
+		for(int i=0; i<chArr.length; i++) {
+			System.out.print(chArr[i] + " ");
+		}
+		
+	}
+}
