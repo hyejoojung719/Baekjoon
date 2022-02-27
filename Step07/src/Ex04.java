286b666f8b58d9b06cc89e1afd6f9f0d3f0f25f4
fixup <fixup719@gmail.com>
2022-02-27 18:32:19 +0900

    ex04 update 다른 풀이도 궁금

--- Step07/src/Ex04.java
+++ Step07/src/Ex04.java
@@ -1,4 +1,25 @@
+import java.util.Scanner;
 
 public class Ex04 {
-
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int testCase = Integer.parseInt(sc.nextLine());
+		
+		String[] strArr = new String[testCase];
+		int[] cntArr = new int[testCase];
+		for(int i=0; i<testCase; i++) {
+			cntArr[i] = Integer.parseInt(sc.nextLine());
+			strArr[i] = sc.nextLine();
+		}
+		
+		for(int i=0; i<testCase; i++) {
+			for(int j=0; j<strArr[i].length();j++) {
+				for(int k=0; k < cntArr[i];k++) {
+					System.out.print(strArr[i].charAt(j));
+				}
+			}
+			System.out.println();
+		}
+	}
 }
