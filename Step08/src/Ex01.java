8e0bf33f4d454e8a4b3ba53b8ae1efccc57b0e8a
fixup <fixup719@gmail.com>
2022-03-05 17:51:32 +0900

    ex01 update 어이가 없당..

--- Step08/src/Ex01.java
+++ Step08/src/Ex01.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
 
 public class Ex01 {
-	 public static void main(String[] args) {
+	 public static void main(String[] args) throws Exception{
+		Scanner sc = new Scanner(System.in);
 		
+		int a = Integer.parseInt(sc.next());
+		int b = Integer.parseInt(sc.next());
+		int c = Integer.parseInt(sc.next());
+		
+		if(c-b < 0) {
+			System.out.println("-1");
+		}else {
+			System.out.println((a/(c-b))+1);
+		}
+
 	}
 }
