a6da049542a60fc1abe0daf84ee4d7f9932d7bcd
fixup <fixup719@gmail.com>
2022-03-23 16:16:41 +0900

    ex05 update 에라토스테네스의 체(풀이참고)

---
+++ Step09/src/Ex05.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ex05 {
+	
+	public static ArrayList<Integer> cnt = new ArrayList<>();
+	public static boolean[] prime;
+	public static void get_prime(int num) {
+		prime = new boolean[2*num+1];
+		
+		prime[0] = prime[1] = true;
+		
+		for(int i=2; i*i <= 2*num; i++) {
+			for(int j=2*i ; j<=2*num; j+=i) prime[j] = true;
+		}
+		
+		int count=0;
+		for(int i=num+1; i<=2*num; i++) {
+			if(!prime[i]) {
+				count++;
+			}
+		}
+		cnt.add(count);
+	}
+	
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		while(true) {
+			int num = sc.nextInt();
+			if(num==0) {
+				break;
+			}
+			get_prime(num);
+		}
+		
+		for(int i=0; i<cnt.size(); i++) {
+			System.out.println(cnt.get(i));
+		}
+	}
+}
