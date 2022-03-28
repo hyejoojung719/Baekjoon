f1197cd2be52b435789ffdd15d05d51d665a8680
fixup <fixup719@gmail.com>
2022-03-28 17:44:35 +0900

    재귀 이해하기

--- Step09/src/RecursionPractice.java
+++ Step09/src/RecursionPractice.java
@@ -1,29 +1,32 @@
 
 public class RecursionPractice {
 	
-	public static int temp;
+	public static int sum;
+	public static int loop = 0;
 	
 	public static int func(int num) {
 		
-		temp = num;
+		sum = num;
+		
 		
 		if(num == -1) {
-			return 0;
+			return 0; // 종료 시점 명시(스택오버플로우 방지)
 		}
 		
-		temp += func(num-1); // temp = temp + num;
+		sum += func(num-1); // sum = sum + num;
 		// func(5)가 실행되고 여기까지 도착하면 func(4).... func(0)까지 진행...
 		// 즉, 이 아래는 func(0)이 실행되고 아래 코드가 실행된다...
-		
-		System.out.println("Test temp: " + temp);
+		loop++;
+		System.out.println("재귀 실행 횟수 : " + loop);
+		System.out.println("Test sum: " + sum);
 		System.out.println("Test num: " + num);
 		System.out.println("--------------------------");
-		return temp;
+		return sum;
 	}
 	
 	public static void main(String[] args) {
 		
 		// 재귀 테스트 
-		func(5);
+		System.out.println(func(5));
 	}
 }
