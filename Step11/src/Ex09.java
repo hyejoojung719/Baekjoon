ea04009737ec5c604991fb94e46795ac66519f26
fixup <fixup719@gmail.com>
2022-03-30 17:06:23 +0900

    update

--- Step11/src/Ex09.java
+++ Step11/src/Ex09.java
@@ -3,39 +3,42 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
-// BOJ 11651 좌표정렬하기
-public class Ex07 {
+//BOJ 10814 나이순정렬
+public class Ex09 {
 	public static void main(String[] args) throws Exception{
+		// 1. 나이 오름차순
+		// 2. 나이 같으면 먼저 가입한 사람순
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
 		
 		int N = Integer.parseInt(br.readLine());
-		int[][] arr = new int[N][2];
+		String[][] arr = new String[N][2];
 		
 		for(int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			
-			arr[i][0] = Integer.parseInt(st.nextToken());
-			arr[i][1] = Integer.parseInt(st.nextToken());
+			arr[i][0] = st.nextToken();
+			arr[i][1] = st.nextToken();
 		}
 		
-		Arrays.sort(arr, (o1, o2) ->{
-			if(o1[1] == o2[1]) {
-				return Integer.compare(o1[0], o2[0]);
-			}else {
-				return Integer.compare(o1[1], o2[1]);
+		Arrays.sort(arr, new Comparator<String[]>() {
+			public int compare(String[] s1, String[] s2) {
+				return Integer.parseInt(s1[0]) - Integer.parseInt(s2[0]);
 			}
 		});
 		
-		for(int i=0; i<N; i++) {
+		for(int i=0; i<arr.length; i++) {
 			bw.write(arr[i][0] +  " " + arr[i][1] + "\n");
 		}
 		
 		bw.flush();
 		bw.close();
 		br.close();
+		
 	}
 }
---
+++ Step11/src/Ex10.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+//BOJ 18870 좌표 압축
+
+//'서로 다른' 이라는 조건 때문에 어려웠던 문제..
+public class Ex10 {
+	public static void main(String[] args) throws Exception {
+		
+		// 1. 중복 제거한 배열 새로 생성
+		// 2. 거기서 좌표 압축 한 숫자 count
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		int[] temp = new int[N];
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		
+		
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i=0; i<cnt.length; i++) {
+			sb.append(cnt[i]).append(' ');
+		}
+		
+		System.out.println(sb);
+		
+		br.close();
+	}
+}
