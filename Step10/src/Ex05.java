2d1a0f2481bf4333371d905dc7ab8c91c76b299e
fixup <fixup719@gmail.com>
2022-03-31 14:49:26 +0900

    update....

---
+++ Step10/src/Ex05.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// BOJ 1436 ¿µÈ­°¨µ¶ ¼ò
+public class Ex05 {
+	public static void main(String[] args) throws Exception  {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		
+	}
+}
