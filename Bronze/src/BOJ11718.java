b713c9080d6f49f4048f20b0731395468b5e7eb6
fixup <fixup719@gmail.com>
2022-04-08 22:26:57 +0900

    boj11718 그대로 출력하기

---
+++ Bronze/src/BOJ11718.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//그대로 출력하기
+// 알고리즘 : 구현, 문자열
+public class BOJ11718 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		while(sc.hasNextLine()) {
+			String str = sc.nextLine();
+			System.out.println(str);
+		}
+		
+		sc.close();
+	}
+}
