e21a1178eeb3acbb5ece7cfd1b1adf14ac063f35
fixup <fixup719@gmail.com>
2022-04-20 14:39:09 +0900

    BOJ2743 단어길이재기

---
+++ Bronze/src/BOJ2743.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+// 단어 길이 재기
+// 알고리즘 : 구현, 문자열
+public class BOJ2743 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.nextLine();
+		
+		System.out.println(str.length());
+	}
+}
