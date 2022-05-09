3696ce239d9cade11050d70c0b1722372aab3526
fixup <fixup719@gmail.com>
2022-05-09 21:02:10 +0900

    BOJ15680 연세대학교 코드리뷰필요x

---
+++ Bronze/src/BOJ15680.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+// 연세대학교
+// 알고리즘 : 구현
+public class BOJ15680 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		if(N==0) {
+			System.out.println("YONSEI");
+		}else {
+			System.out.println("Leading the Way to the Future");
+		}
+		
+		
+	}
+}
