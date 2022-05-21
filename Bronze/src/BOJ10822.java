84dbb4d66af64ec0581c57467c0bd58ae62d8d82
fixup <fixup719@gmail.com>
2022-05-21 23:37:33 +0900

    BOJ10822

---
+++ Bronze/src/BOJ10822.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+// 더하기
+// 알고리즘 : 수학, 구현, 문자열, 사직연산, 파싱
+public class BOJ10822 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.next();
+		
+		String[] arr = str.split(",");
+	
+		int sum = 0;
+		for(int i=0; i<arr.length; i++) {
+			sum += Integer.parseInt(arr[i]);
+		}
+		
+		System.out.println(sum);
+	}
+
+}
